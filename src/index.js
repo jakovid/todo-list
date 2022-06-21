1453890c3502e15b92f88c2eaa4ca210dacd05ae
jakovid <jacobk.dowdle@gmail.com>
2022-06-21 15:22:32 +0800

    deleted everything because it was terrible

--- src/index.js
+++ src/index.js
@@ -4,27 +4,7 @@ import { header } from "./header";
 (function() {
     var todoModule = {
         init: function() {
-            header.buildHeader();
-            todo.buildTodo();
-            footer.buildFooter();
-            this.cacheDom();
-            this.bindEvents();
-        },
-        cacheDom: function() {
-            this.el = document.getElementById('todoModule');
-            console.log(this.el);
-            this.todoNavAdd = this.el.querySelector('#todoNavAdd');
-        },
-        bindEvents: function() {
-            //need to move this to the todo.js
-            this.todoNavAdd.onclick = this.projectInput;
-        },
-        projectInput: function() {
-            todo.projectInput();
-            todoModule.cacheDom();
-            todoModule.bindEvents();
-        }
-        
+        }        
     };
 
     todoModule.init();
