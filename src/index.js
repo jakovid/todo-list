a072ad013fa3c9029f66260b0158e1355dc50726
jakovid <jacobk.dowdle@gmail.com>
2022-06-21 16:41:31 +0800

    added a cancel function to the add new project event

--- src/index.js
+++ src/index.js
@@ -3,9 +3,46 @@
     var todoModule = {
         init: function() {
             this.cacheDom();
+            this.addProjectBtn();
         },
         cacheDom: function(){
             this.el = document.getElementById('todoModule');
+            this.newProjects = this.el.querySelector('.newProjects');
+            this.addProjectsBox = this.el.querySelector('.addProjectBox')
+        },
+        addProjectBtn: function(){
+            todoModule.addProjectsBox.innerHTML = '';
+            //create element
+            let addBtn = document.createElement('div');
+            //add id
+            addBtn.id = 'addBtn';
+            //add innerText
+            addBtn.innerText = 'Add New Project';
+            //add event listener
+            addBtn.addEventListener('click',todoModule.addNewProject);
+            //append element
+            todoModule.addProjectsBox.appendChild(addBtn);
+        },
+        addNewProject: function() {
+            //bye bye add new project
+            todoModule.addProjectsBox.innerHTML = '';
+            //create elements
+            let projectInput = document.createElement('input');
+            let btnBox = document.createElement('div');
+            let confirmAdd = document.createElement('button');
+            let cancelAdd = document.createElement('button');
+            //add class
+            projectInput.className = 'projectInput';
+            confirmAdd.className = 'newProjectBtn';
+            cancelAdd.className = 'newProjectBtn';
+            //add innerText
+            confirmAdd.innerText = 'Add';
+            cancelAdd.innerText = 'Cancel';
+            //add eventListners
+            cancelAdd.addEventListener('click',todoModule.addProjectBtn);
+            //append
+            btnBox.append(confirmAdd,cancelAdd);
+            todoModule.addProjectsBox.append(projectInput,btnBox);
         }
 
     };
